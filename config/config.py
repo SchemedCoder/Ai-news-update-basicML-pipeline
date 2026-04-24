d57f43caf8ad93d2a10d2a4841a209cf547cc035
Navin Tigga <146591653+SchemedCoder@users.noreply.github.com>
2026-04-25 00:17:07 +0530

    Create config.py

---
+++ config/config.py
@@ -0,0 +1,12 @@
+API_KEY = "YOUR_API_KEY"
+
+DB_CONFIG = {
+    "dbname": "ai_news",
+    "user": "postgres",
+    "password": "password",
+    "host": "localhost",
+    "port": "5432"
+}
+
+KAFKA_BROKER = "localhost:9092"
+TOPIC = "ai_news"
